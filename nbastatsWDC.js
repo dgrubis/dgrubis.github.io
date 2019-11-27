b1a88a4a5ec9b840078303f8d2ee5b55a48aecfd
Derek Grubis <dgrubis@Dereks-MacBook-Pro.local>
2019-11-27 14:48:09 -0500

    debug json parser

--- nbastatsWDC.js
+++ nbastatsWDC.js
@@ -3,24 +3,13 @@
 
     myConnector.getSchema = function (schemaCallback) {
     var cols = [{
-        id: "DEF_RATING",
-        dataType: tableau.dataTypeEnum.float
-    }, {
-        id: "mag",
-        alias: "magnitude",
-        dataType: tableau.dataTypeEnum.float
-    }, {
-        id: "title",
-        alias: "title",
+        id: "Player",
         dataType: tableau.dataTypeEnum.string
-    }, {
-        id: "location",
-        dataType: tableau.dataTypeEnum.geometry
     }];
 
     var tableSchema = {
-        id: "earthquakeFeed",
-        alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+        id: "nbastatsdefense",
+        alias: "Defesnive Data",
         columns: cols
     };
 
@@ -29,16 +18,13 @@
 
 myConnector.getData = function(table, doneCallback) {
 $.getJSON("https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Defense&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2019-20&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=", function(resp) {
-    var feat = resp.features,
+    var feat = resp.resultSets.rowSet,
         tableData = [];
 
     // Iterate over the JSON object
     for (var i = 0, len = feat.length; i < len; i++) {
         tableData.push({
-            "DEF_RATING": feat[i].properties.DEF_RATING,
-            "mag": feat[i].properties.mag,
-            "title": feat[i].properties.title,
-            "location": feat[i].geometry
+            "Player": feat[i].0
         });
     }
 
