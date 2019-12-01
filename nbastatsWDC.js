f2b19cf84aae46cd402f0f995f7756171ddc961f
Derek Grubis <dgrubis@Dereks-MacBook-Pro.local>
2019-12-01 13:09:49 -0500

    reverted back to example

--- nbastatsWDC.js
+++ nbastatsWDC.js
@@ -3,8 +3,19 @@
 
     myConnector.getSchema = function (schemaCallback) {
     var cols = [{
-        id: "PLAYER_NAME",
-        dataType: tableau.dataTypeEnum.string
+      id: "id",
+      dataType: tableau.dataTypeEnum.string
+  }, {
+      id: "mag",
+      alias: "magnitude",
+      dataType: tableau.dataTypeEnum.float
+  }, {
+      id: "title",
+      alias: "title",
+      dataType: tableau.dataTypeEnum.string
+  }, {
+      id: "location",
+      dataType: tableau.dataTypeEnum.geometry
     }];
 
     var tableSchema = {
@@ -17,14 +28,17 @@
 };
 
 myConnector.getData = function(table, doneCallback) {
-$.getJSON("https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Defense&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2019-20&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=", function(resp) {
-    var feat = resp.resultSets[0].rowSet,
+$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+    var feat = resp.features,
         tableData = [];
 
     // Iterate over the JSON object
     for (var i = 0, len = feat.length; i < len; i++) {
         tableData.push({
-            "PLAYER_NAME": feat[i][1]
+          "id": feat[i].id,
+          "mag": feat[i].properties.mag,
+          "title": feat[i].properties.title,
+          "location": feat[i].geometry
         });
     }
 
