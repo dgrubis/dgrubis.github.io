731b37a3f81a74b38c75d94ae3e264484ba8cff3
Derek Grubis <dgrubis@Dereks-MacBook-Pro.local>
2019-12-01 13:33:14 -0500

    test on parameters

--- nbastatsWDC.js
+++ nbastatsWDC.js
@@ -5,17 +5,6 @@
     var cols = [{
       id: "id",
       dataType: tableau.dataTypeEnum.string
-  }, {
-      id: "mag",
-      alias: "magnitude",
-      dataType: tableau.dataTypeEnum.float
-  }, {
-      id: "title",
-      alias: "title",
-      dataType: tableau.dataTypeEnum.string
-  }, {
-      id: "location",
-      dataType: tableau.dataTypeEnum.geometry
     }];
 
     var tableSchema = {
@@ -29,18 +18,15 @@
 
 myConnector.getData = function(table, doneCallback) {
 $.getJSON("https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Defense&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2019-20&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=", function(resp) {
-    var feat = resp.features,
+    var feat = resp.parameters,
         tableData = [];
 
     // Iterate over the JSON object
-    for (var i = 0, len = feat.length; i < len; i++) {
+    //for (var i = 0, len = feat.length; i < len; i++) {
         tableData.push({
-          "id": feat[i].id,
-          "mag": feat[i].properties.mag,
-          "title": feat[i].properties.title,
-          "location": feat[i].geometry
+          "id": feat.MeasureType
         });
-    }
+    //}
 
     table.appendRows(tableData);
     doneCallback();
